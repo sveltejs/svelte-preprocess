0ac5cc116f879f62805e507f4bca6ccac9912612
Christian Kaisermann <christian@kaisermann.me>
2018-05-15 11:53:11 -0300

    Rename some type declarations

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,26 +1,24 @@
-type SveltePreprocessObj = {
+type SveltePreprocessObject = {
   markup: Function,
   script: Function,
   style: Function
 }
 
-type PreprocessedObj = {
+type PreprocessResult = {
   code: string;
   map?: object;
 }
 
-
-
-type MagicSvelteOptions = {
+type SveltePreprocessOptions = {
   onBefore?({ content, filename }: { content: string, filename: string }): string;
   languages?: {
     [language: string]: boolean | object | (({ content, filename }: { content: string, filename?: string }) =>
-    PreprocessedObj | Promise<PreprocessedObj>)
+    PreprocessResult | Promise<PreprocessResult>)
   }
 }
 
-declare function magicalPreprocess(config: MagicSvelteOptions): SveltePreprocessObj;
+declare function getSveltePreprocessor(config: SveltePreprocessOptions): SveltePreprocessObject;
 
 declare module "svelte-preprocess" {
-  export = magicalPreprocess;
+  export = getSveltePreprocessor;
 }
