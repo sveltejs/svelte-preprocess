55e9d28fd03a2a1bf07c4d1b9ec3517fe2ce0cb3
Christian Kaisermann <christian@kaisermann.me>
2019-07-01 11:41:40 -0300

    fix: 🐛 pass less @imports as dependencies to svelte

--- src/transformers/less.js
+++ src/transformers/less.js
@@ -1,13 +1,15 @@
 const less = require('less/lib/less-node')
 
-module.exports = ({ content, filename, options }) => {
-  return less
-    .render(content, {
-      sourceMap: {},
-      ...options,
-    })
-    .then(output => ({
-      code: output.css,
-      map: output.map,
-    }))
+module.exports = async ({ content, filename, options }) => {
+  const { css, map, imports } = await less.render(content, {
+    sourceMap: {},
+    filename,
+    ...options,
+  })
+
+  return {
+    code: css,
+    map,
+    dependencies: imports,
+  }
 }
