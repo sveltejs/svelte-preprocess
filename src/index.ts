bca41918262df34f4346aa5bdc15e7ed0690ca26
Christian Kaisermann <christian@kaisermann.me>
2019-10-30 17:23:35 -0300

    refactor: 💡 remove unneeded extra functions

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,15 @@
-import { Options } from './typings';
 import { autoPreprocess } from './autoProcess';
 
-import pugProcessor from './processors/pug';
-import coffeescriptProcessor from './processors/coffeescript';
-import typescriptProcessor from './processors/typescript';
-import lessProcessor from './processors/less';
-import scssProcessor from './processors/scss';
-import stylusProcessor from './processors/stylus';
-import postcssProcessor from './processors/postcss';
-import globalStyleProcessor from './processors/globalStyle';
-
-// crazy es6/cjs export mix for backward compatibility
-
 // default auto processor
+// crazy es6/cjs export mix for backward compatibility
 export default exports = module.exports = autoPreprocess;
 
 // stand-alone processors to be included manually */
-// Markup
-export const pug = (opts: Options.Pug) => pugProcessor(opts);
-
-// Script
-export const coffeescript = (opts: Options.Coffeescript) =>
-  coffeescriptProcessor(opts);
-export const typescript = (opts: Options.Typescript) =>
-  typescriptProcessor(opts);
-
-// Style
-export const less = (opts: Options.Less) => lessProcessor(opts);
-export const scss = (opts: Options.Sass) => scssProcessor(opts);
-export const sass = (opts: Options.Sass) => scssProcessor(opts);
-export const stylus = (opts: Options.Stylus) => stylusProcessor(opts);
-export const postcss = (opts: Options.Postcss) => postcssProcessor(opts);
-export const globalStyle = () => globalStyleProcessor();
+export { default as pug } from './processors/pug';
+export { default as coffeescript } from './processors/coffeescript';
+export { default as typescript } from './processors/typescript';
+export { default as less } from './processors/less';
+export { default as scss, default as sass } from './processors/scss';
+export { default as stylus } from './processors/stylus';
+export { default as postcss } from './processors/postcss';
+export { default as globalStyle } from './processors/globalStyle';
