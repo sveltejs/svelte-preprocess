95585c6a0b516bc03a8e6bccf9996d81adc971be
Leonidaz <leonid@Leonids-MBP.fios-router.home>
2020-03-20 03:45:18 -0400

    add tests for renderSync

--- test/transformers/scss.test.ts
+++ test/transformers/scss.test.ts
@@ -4,7 +4,7 @@ import getAutoPreprocess from '../../src';
 import { preprocess } from '../utils';
 
 describe('transformer - scss', () => {
-  it('should prepend scss content via `data` option property', async () => {
+  it('should prepend scss content via `data` option property - via defaul async render', async () => {
     const template = `<style lang="scss"></style>`;
     const opts = getAutoPreprocess({
       scss: {
@@ -15,7 +15,7 @@ describe('transformer - scss', () => {
     expect(preprocessed.toString()).toContain('red');
   });
 
-  it('should return @imported files as dependencies', async () => {
+  it('should return @imported files as dependencies - via default async render', async () => {
     const template = `<style lang="scss">@import "fixtures/style.scss";</style>`;
     const opts = getAutoPreprocess();
     const preprocessed = await preprocess(template, opts);
@@ -23,4 +23,29 @@ describe('transformer - scss', () => {
       resolve(__dirname, '..', 'fixtures', 'style.scss').replace(/[\\/]/g, '/'),
     );
   });
+
+  it('should prepend scss content via `data` option property - via renderSync', async () => {
+    const template = `<style lang="scss"></style>`;
+    const opts = getAutoPreprocess({
+      scss: {
+        data: '$color:blue;div{color:$color}',
+        renderSync: true,
+      },
+    });
+    const preprocessed = await preprocess(template, opts);
+    expect(preprocessed.toString()).toContain('blue');
+  });
+
+  it('should return @imported files as dependencies - via renderSync', async () => {
+    const template = `<style lang="scss">@import "fixtures/style.scss";</style>`;
+    const opts = getAutoPreprocess({
+      scss: {
+        renderSync: true,
+      },
+    });
+    const preprocessed = await preprocess(template, opts);
+    expect(preprocessed.dependencies).toContain(
+      resolve(__dirname, '..', 'fixtures', 'style.scss').replace(/[\\/]/g, '/'),
+    );
+  });  
 });
