fd86c9cca65af83f6c910406f57528a27a79c11c
Christian Kaisermann <christian@kaisermann.me>
2020-06-06 19:27:22 -0300

    docs: ✏️ fix less test

--- test/transformers/less.test.ts
+++ test/transformers/less.test.ts
@@ -4,9 +4,7 @@ import getAutoPreprocess from '../../src';
 import { preprocess } from '../utils';
 
 describe('transformer - less', () => {
-  // TODO: waiting for https://github.com/less/less.js/issues/3508
-  // eslint-disable-next-line jest/no-disabled-tests
-  it.skip('should return @imported files as dependencies', async () => {
+  it('should return @imported files as dependencies', async () => {
     const template = `<style lang="less">@import "fixtures/style.less";</style>`;
     const opts = getAutoPreprocess();
     const preprocessed = await preprocess(template, opts);
