174c4068bad87d6bcb665cc51bd04d1ae367d855
illright <leva181777@gmail.com>
2020-06-06 17:49:35 +0300

    fix: teach the globalifySelector to determine combinators

--- src/modules/globalifySelector.ts
+++ src/modules/globalifySelector.ts
@@ -1,9 +1,19 @@
+const combinatorPattern = /(\s*[ >+~]\s*)(?![^[]+\])/g;
+
 export function globalifySelector(selector: string) {
   return selector
     .trim()
-    .split(' ')
-    .filter(Boolean)
-    .map((selectorPart) => {
+    .split(combinatorPattern)
+    .map((selectorPart: string, index: number) => {
+      // if this is the separator
+      if (index % 2 !== 0) {
+        return selectorPart;
+      }
+
+      if (selectorPart === '') {
+        return selectorPart;
+      }
+
       if (selectorPart.startsWith(':local')) {
         return selectorPart.replace(/:local\((.+?)\)/g, '$1');
       }
@@ -13,5 +23,5 @@ export function globalifySelector(selector: string) {
 
       return `:global(${selectorPart})`;
     })
-    .join(' ');
+    .join('');
 }
