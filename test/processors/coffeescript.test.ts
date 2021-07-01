13e2251e1057bc4603493e2413d770790c768619
Mayke Freitas <mayke.sargento@gmail.com>
2021-07-01 10:32:56 -0300

    preventing typescript from change other languages

--- test/processors/coffeescript.test.ts
+++ test/processors/coffeescript.test.ts
@@ -13,6 +13,18 @@ describe(`processor - coffeescript`, () => {
     expect(preprocessed.toString()).toBe(template);
   });
 
+  it('should leave other languages untouched', async () => {
+    const template = `<script lang="potato">🥔</script>`;
+    const options = {
+      stripIndent: true,
+      prependData: '/* potato */',
+    };
+
+    const preprocessed = await preprocess(template, [coffeescript(options)]);
+
+    expect(preprocessed.toString()).toBe(template);
+  });
+
   it('should support external src files', async () => {
     const template = `<script src="./fixtures/script.coffee"></script>`;
     const options = {};
