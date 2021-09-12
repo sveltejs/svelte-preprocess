c7947035cd2c7e2208c3c3767e5788425963d705
Simon Holthausen <simon.holthausen@accso.de>
2021-09-12 14:59:26 +0200

    (fix) return default settings when no tsconfig found

--- src/transformers/typescript.ts
+++ src/transformers/typescript.ts
@@ -351,7 +351,7 @@ export function loadTsconfig(
     ts.findConfigFile(fileDirectory, ts.sys.fileExists);
 
   if (!tsconfigFile) {
-    return { errors: [], options: {} };
+    return { errors: [], options: compilerOptionsJSON };
   }
 
   tsconfigFile = isAbsolute(tsconfigFile)
