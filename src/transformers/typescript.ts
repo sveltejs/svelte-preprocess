85a86c86513f98c2b02f2d314a18b0e68378c8f6
Simon H <5968653+dummdidumm@users.noreply.github.com>
2021-12-16 13:04:10 +0100

    fix: adjust store regex (#441)
    Don't match in the case of $° or $§ or $\, these are not store variables
    
    Fixes #433

--- src/transformers/typescript.ts
+++ src/transformers/typescript.ts
@@ -152,7 +152,8 @@ function injectVarsToCode({
   // TODO investigate if it's possible to achieve this with a
   // TS transformer (previous attemps have failed)
   const codestores = Array.from(
-    contentForCodestores.match(/\$[^\s();:,[\]{}.?!+-=*/~|&%<>^`"']+/g) || [],
+    contentForCodestores.match(/\$[^\s();:,[\]{}.?!+-=*/\\~|&%<>^`"'°§]+/g) ||
+      [],
     (name) => name.slice(1),
   ).filter((name) => !JAVASCRIPT_RESERVED_KEYWORD_SET.has(name));
 
