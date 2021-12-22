620b24a83d3bbdc1ce878bf9b0c71662adf3c457
Mateusz Burzyński <mateuszburzynski@gmail.com>
2021-12-22 13:05:39 +0100

    add comment for `supportsTopLevelAwait: true`

--- src/transformers/babel.ts
+++ src/transformers/babel.ts
@@ -24,6 +24,7 @@ const transformer: Transformer<Options.Babel> = async ({
       name: 'svelte-preprocess',
       supportsStaticESM: true,
       supportsDynamicImport: true,
+      // this isn't supported by Svelte but let it error with a good error on this syntax untouched
       supportsTopLevelAwait: true,
       // this can be enabled once all "peer deps" understand this
       // this syntax is supported since rollup@1.26.0 and webpack@5.0.0-beta.21
