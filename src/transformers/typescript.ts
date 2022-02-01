10218717fd2b21721ade0392e50cb67ff977fdca
Andre Henriques <andr3h3nriqu3s@gmail.com>
2022-02-01 12:48:36 +0000

    fix: add # and numbers to ts store regex
    Related with #466

--- src/transformers/typescript.ts
+++ src/transformers/typescript.ts
@@ -152,8 +152,9 @@ function injectVarsToCode({
   // TODO investigate if it's possible to achieve this with a
   // TS transformer (previous attemps have failed)
   const codestores = Array.from(
-    contentForCodestores.match(/\$[^\s();:,[\]{}.?!+\-=*/\\~|&%<>^`"'°§]+/g) ||
-      [],
+    contentForCodestores.match(
+      /\$[^\s();:,[\]{}.?!+\-=*/\\~|&%<>^`"'°§#1-9]+/g,
+    ) || [],
     (name) => name.slice(1),
   ).filter((name) => !JAVASCRIPT_RESERVED_KEYWORD_SET.has(name));
 
