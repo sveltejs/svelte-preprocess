da8c01c763a4a8e0ccb97f1e0a9c7d1a9aa48730
Christian Kaisermann <christian@kaisermann.me>
2022-12-10 13:37:24 +0100

    chore: update linter and formatter

--- test/modules/globalifySelector.test.ts
+++ test/modules/globalifySelector.test.ts
@@ -4,66 +4,56 @@ describe('globalifySelector', () => {
   it('correctly treats CSS selectors with legal spaces', async () => {
     const selector = '[attr="with spaces"]';
 
-    expect(globalifySelector(selector)).toEqual(
-      ':global([attr="with spaces"])',
-    );
+    expect(globalifySelector(selector)).toBe(':global([attr="with spaces"])');
   });
 
   it('works with combinators', async () => {
-    expect(globalifySelector('ul + p')).toEqual(`:global(ul) + :global(p)`);
-    expect(globalifySelector('p > a')).toEqual(`:global(p) > :global(a)`);
-    expect(globalifySelector('p + p')).toEqual(`:global(p) + :global(p)`);
-    expect(globalifySelector('li a')).toEqual(`:global(li) :global(a)`);
-    expect(globalifySelector('div ~ a')).toEqual(`:global(div) ~ :global(a)`);
-    expect(globalifySelector('div, a')).toEqual(`:global(div), :global(a)`);
+    expect(globalifySelector('ul + p')).toBe(`:global(ul) + :global(p)`);
+    expect(globalifySelector('p > a')).toBe(`:global(p) > :global(a)`);
+    expect(globalifySelector('p + p')).toBe(`:global(p) + :global(p)`);
+    expect(globalifySelector('li a')).toBe(`:global(li) :global(a)`);
+    expect(globalifySelector('div ~ a')).toBe(`:global(div) ~ :global(a)`);
+    expect(globalifySelector('div, a')).toBe(`:global(div), :global(a)`);
   });
 
   it('correctly treats selectors with escaped combinator characters', async () => {
     const selector1 = '.\\~positive.\\!normal ~ .\\+foo';
 
-    expect(globalifySelector(selector1)).toEqual(
+    expect(globalifySelector(selector1)).toBe(
       ':global(.\\~positive.\\!normal) ~ :global(.\\+foo)',
     );
   });
 
   it('works with nth-child', async () => {
-    expect(globalifySelector('tr:nth-child(odd)')).toEqual(
+    expect(globalifySelector('tr:nth-child(odd)')).toBe(
       `:global(tr:nth-child(odd))`,
     );
-    expect(globalifySelector('tr:nth-child(2n+1)')).toEqual(
+    expect(globalifySelector('tr:nth-child(2n+1)')).toBe(
       `:global(tr:nth-child(2n+1))`,
     );
-    expect(globalifySelector('tr:nth-child(even)')).toEqual(
+    expect(globalifySelector('tr:nth-child(even)')).toBe(
       `:global(tr:nth-child(even))`,
     );
-    expect(globalifySelector('tr:nth-child(2n)')).toEqual(
+    expect(globalifySelector('tr:nth-child(2n)')).toBe(
       `:global(tr:nth-child(2n))`,
     );
-    expect(globalifySelector(':nth-child(7)')).toEqual(
-      `:global(:nth-child(7))`,
-    );
-    expect(globalifySelector(':nth-child(5n)')).toEqual(
-      `:global(:nth-child(5n))`,
-    );
-    expect(globalifySelector(':nth-child(n+7)')).toEqual(
+    expect(globalifySelector(':nth-child(7)')).toBe(`:global(:nth-child(7))`);
+    expect(globalifySelector(':nth-child(5n)')).toBe(`:global(:nth-child(5n))`);
+    expect(globalifySelector(':nth-child(n+7)')).toBe(
       `:global(:nth-child(n+7))`,
     );
-    expect(globalifySelector(':nth-child(3n+4)')).toEqual(
+    expect(globalifySelector(':nth-child(3n+4)')).toBe(
       `:global(:nth-child(3n+4))`,
     );
-    expect(globalifySelector(':nth-child(-n+3)')).toEqual(
+    expect(globalifySelector(':nth-child(-n+3)')).toBe(
       `:global(:nth-child(-n+3))`,
     );
-    expect(globalifySelector('p:nth-child(n)')).toEqual(
-      `:global(p:nth-child(n))`,
-    );
-    expect(globalifySelector('p:nth-child(1)')).toEqual(
-      `:global(p:nth-child(1))`,
-    );
-    expect(globalifySelector('p:nth-child(0n+1)')).toEqual(
+    expect(globalifySelector('p:nth-child(n)')).toBe(`:global(p:nth-child(n))`);
+    expect(globalifySelector('p:nth-child(1)')).toBe(`:global(p:nth-child(1))`);
+    expect(globalifySelector('p:nth-child(0n+1)')).toBe(
       `:global(p:nth-child(0n+1))`,
     );
-    expect(globalifySelector('p:nth-child(n+8):nth-child(-n+15)')).toEqual(
+    expect(globalifySelector('p:nth-child(n+8):nth-child(-n+15)')).toBe(
       `:global(p:nth-child(n+8):nth-child(-n+15))`,
     );
   });
