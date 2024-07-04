ec106126546f267d874c575911de66405501f2a4
Ben McCann <322311+benmccann@users.noreply.github.com>
2024-07-04 15:48:09 -0700

    chore: remove strip-indent dependency

--- src/modules/prepareContent.ts
+++ src/modules/prepareContent.ts
@@ -1,6 +1,4 @@
-import stripIndent from 'strip-indent';
-
-// todo: could use magig-string and generate some sourcemaps 🗺
+// todo: could use magic-string and generate some sourcemaps 🗺
 export function prepareContent({
   options,
   content,
@@ -21,4 +19,28 @@ export function prepareContent({
   }
 
   return content;
+}
+
+/** Get the shortest leading whitespace from lines in a string */
+const minIndent = (s: string) => {
+  const match = s.match(/^[ \t]*(?=\S)/gm);
+
+  if (!match) {
+    return 0;
+  }
+
+  return match.reduce((r, a) => Math.min(r, a.length), Infinity);
+};
+
+/** Strip leading whitespace from each line in a string */
+function stripIndent(s: string) {
+  const indent = minIndent(s);
+
+  if (indent === 0) {
+    return s;
+  }
+
+  const regex = new RegExp(`^[ \\t]{${indent}}`, 'gm');
+
+  return s.replace(regex, '');
 }
--- src/types/modules.d.ts
+++ src/types/modules.d.ts
@@ -1,6 +1,5 @@
 declare module 'svelte/package.json';
 declare module 'coffeescript';
-declare module 'strip-indent';
 declare module 'postcss-load-config';
 declare module 'less';
 declare module 'sorcery';
